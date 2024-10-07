8143ee4e991bbd2746ebd3564eea785bed5bf661
armandonum <armandonunez404@gamil.com>
2024-10-07 17:32:48 -0400

    first commint

---
+++ meme-generator/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import MemeGenerator from './components/MemeGenerator.tsx';
+function App() {
+  return (
+    <div className="App">
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
